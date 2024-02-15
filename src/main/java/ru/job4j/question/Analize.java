81bb844c8e5cfa7ca54fc6e3be02ebe5cf2a05c3
andreisemin <andr-syomin@yandex.ru>
2024-02-15 17:51:27 +0400

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,30 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added = 0;
+        int changed = 0;
+        int deleted = 0;
+        Map<Integer, String> previousMap = new HashMap<>();
+        for (User user : previous) {
+            previousMap.put(user.getId(), user.getName());
+        }
+        for (User user : current) {
+            String previousName = previousMap.get(user.getId());
+            if (previousName == null) {
+                added++;
+            } else if (!previousName.equals(user.getName())) {
+                changed++;
+            }
+            previousMap.remove(user.getId());
+        }
+        deleted = previousMap.size();
+        return new Info(added, changed, deleted);
+    }
+
+}
